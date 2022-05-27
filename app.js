6fc078fb2aaafb8d27c1c5e6f788046d4212e73d
caique.moraes <caique.moraes@compasso.com.br>
2022-05-27 10:57:31 -0300

    adicionando livereload

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const path = require('path')
 const rootRouter = require('./src/routes/root')
+
+
+const port = 3003
+const link = `http://localhost:${port}`
+
 const app = express()
+
 
 app.set('views', path.join(__dirname, './src/views'))
 app.set('view engine', 'ejs')
-
 app.use(express.static(__dirname + '/public'))
 app.use(expressLayouts)
 app.use(rootRouter)
+app.use(require('easy-livereload')())
 
-app.listen(4000, () => console.log('servidor iniciado'))
+app.listen(port, () => console.log(`Aplicação iniciada em: ${link}: `))
